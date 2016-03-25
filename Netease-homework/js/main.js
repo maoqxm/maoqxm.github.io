2da2e492fd89a99f2df82b8c27eb0daf1456cf2f
毛球 <392818909@qq.com>
2016-03-25 20:45:57 +0800

    combine js

--- Netease-homework/js/main.js
+++ Netease-homework/js/main.js
@@ -1,3 +1,134 @@
+// banner
+// 根据实际页面动态生成容器高度
+function bannerHeight(){
+    var img = imgArr[0];
+    var rightHeight = window.getComputedStyle(img).getPropertyValue('height');
+    banner.style.height = rightHeight;
+}
+// 判断是否有cls类
+function hasClass(obj, cls){
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    return obj.className.match(reg);
+}
+// 增加cls类
+function addClass(obj, cls){
+    if (!hasClass(obj,cls)) {
+        obj.className += ' ' + cls;
+    }
+}
+// 移除cls类
+function removeClass(obj, cls){
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    obj.className = obj.className.replace(reg, ''); // 这里有个小问题就是 如果是"aa bb cc".replace(" bb ", '')这种情况就会导致aa和cc之间没有空格
+}
+// 改变图片和小圆点
+function changeTo(num){
+    var curImg = document.getElementsByClassName('show')[0];
+    var tarImg = imgArr[num];
+    removeClass(curImg, 'show');
+    addClass(tarImg, 'show');
+    var curIndex = document.getElementsByClassName('active')[0];
+    var tarIndex = indexArr[num];
+    removeClass(curIndex, 'active');
+    addClass(tarIndex, 'active');
+}
+// 小圆点点击事件监听
+function indexClick(){
+    for (i = 0; i < imgLen; i++) {
+        (function(_i){
+            indexArr[_i].addEventListener('click', function(){
+                changeTo(_i);
+                curIndex = _i;
+            },false);
+        })(i)
+    }
+}
+// 停止自动播放
+function stop(){
+    clearInterval(autoChange);
+}
+
+var banner = document.getElementById('banner');
+var imgArr = document.getElementById('list').getElementsByTagName('li');
+var imgLen = imgArr.length;
+var indexArr = document.getElementById('pointer').getElementsByTagName('li');
+var curIndex = 0;
+var autoChange = setInterval(function(){
+    if (curIndex < imgLen - 1){
+        curIndex++;
+    }else{
+        curIndex = 0;
+    }
+    changeTo(curIndex);
+}, 5000);
+
+(function(){
+    bannerHeight();
+    indexClick();
+    banner.addEventListener('mouseover', stop, false);
+    banner.addEventListener('mouseout', function(){
+        autoChange = setInterval(function(){
+            if (curIndex < imgLen - 1) {
+                curIndex++;
+            }else {
+                curIndex = 0;
+            }
+            changeTo(curIndex);
+        },5000);
+    }, false);
+    window.addEventListener('resize',bannerHeight,false);
+})()
+
+//cookie
+
+// 设置cookie
+function setCookie (name, value, expires, path, domain, secure){
+    var cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+    if (expires){
+        var today = new Date();
+        today.setDate(today.getDate() + expires);
+        cookie += '; expires=' + today.toGMTString();
+    }
+    if (path)
+        cookie += '; path=' + path;
+    if (domain)
+        cookie += '; domain=' + domain;
+    if (secure)
+        cookie += '; secure=' + secure;
+    document.cookie = cookie;
+}
+// 解析cookie
+function getcookie (){
+    var cookie = {};
+    var all = document.cookie;
+    if (all === '')
+        return cookie;
+    var list = all.split('; ');
+    for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        var p = item.indexOf('=');
+        var name = item.substring(0, p);
+        name = decodeURIComponent(name);
+        var value = item.substring(p + 1);
+        value = decodeURIComponent(value);
+        cookie[name] = value;
+    }
+    return cookie;
+}
+// 点击不再提醒后设置本地cookie
+var noRemind = document.getElementById("tip_noremind");
+noRemind.addEventListener('click', function(){
+    setCookie("noRemind", "1", 365);
+    tip.style.display = "none";
+}, false)
+// 查询是否已有noRemind的cookie
+var tip = document.getElementById("tip");
+var mycookie = getcookie();
+if (mycookie.noRemind) {
+    tip.style.display = "none";
+}
+
+
 /*
 封装Ajax方法
 参数说明：
